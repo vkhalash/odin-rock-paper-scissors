8e08f684320be22ab3059904c55146c0d6900722
Vitalii Khalash <144526955+vkhalash@users.noreply.github.com>
2025-10-02 19:52:10 +0300

    Add playGame function
    Implement the main game loop with multiple rounds.
    Track and compare scores between user and computer.
    Declare the overall winner at the end of the game.

--- script.js
+++ script.js
@@ -36,22 +36,47 @@ const rules = {
   scissors: "paper",
 };
 
-function playRound(computerChoice, userChoice) {
-  let roundResult;
+function playGame(roundCount = 5) {
+  let computerScore = 0;
+  let userScore = 0;
 
-  if (computerChoice === userChoice) {
-    roundResult = "Tie";
-  } else if (rules[userChoice] === computerChoice) {
-    roundResult = "You won the round";
-  } else {
-    roundResult = "Computer won the round";
+  function playRound(round, computerChoice, userChoice) {
+    let roundResult;
+
+    if (computerChoice === userChoice) {
+      roundResult = "Tie";
+    } else if (rules[userChoice] === computerChoice) {
+      roundResult = "You won the round";
+      userScore++;
+    } else {
+      roundResult = "Computer won the round";
+      computerScore++;
+    }
+
+    const roundMessage = `Round ${round}: ${roundResult}. You chose ${userChoice}, computer chose ${computerChoice}.`;
+    console.log(roundMessage);
+    alert(roundMessage);
   }
 
-  const roundMessage = `${roundResult}. You chose ${userChoice}, computer chose ${computerChoice}.`;
-  console.log(roundMessage);
-  alert(roundMessage);
+  for (let round = 1; round <= roundCount; round++) {
+    const computerChoice = getComputerChoice();
+    const userChoice = getUserChoice();
+    playRound(round, computerChoice, userChoice);
+  }
+
+  let gameResult;
+
+  if (computerScore === userScore) {
+    gameResult = "Tie";
+  } else if (userScore > computerScore) {
+    gameResult = "You won the game";
+  } else {
+    gameResult = "Computer won the game";
+  }
+
+  const gameMessage = `${gameResult}. Score: ${userScore}-${computerScore}.`;
+  console.log(gameMessage);
+  alert(gameMessage);
 }
 
-const computerChoice = getComputerChoice();
-const userChoice = getUserChoice();
-playRound(computerChoice, userChoice);
+playGame();
