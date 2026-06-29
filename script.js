81477f9acd6e83b7ba33d60e082c23ba6e085888
Vitalii Khalash <144526955+vkhalash@users.noreply.github.com>
2026-06-29 19:10:18 +0300

    Add buttons to make player selections

--- script.js
+++ script.js
@@ -11,78 +11,33 @@ function getComputerChoice() {
   }
 }
 
-function getUserChoice() {
-  while (true) {
-    const userChoice = prompt('Enter "rock", "paper", or "scissors":')
-      ?.trim()
-      .toLowerCase();
-
-    if (!userChoice) {
-      alert("Input cannot be empty.");
-      continue;
-    }
-
-    if (["rock", "paper", "scissors"].includes(userChoice)) {
-      return userChoice;
-    }
-
-    alert("Invalid input. Try again.");
-  }
-}
-
 const rules = {
   rock: "scissors",
   paper: "rock",
   scissors: "paper",
 };
 
-function playGame(roundCount = 5) {
-  let computerScore = 0;
-  let userScore = 0;
+const buttons = document.querySelectorAll("button");
 
-  function playRound(round, computerChoice, userChoice) {
-    let roundResult;
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.dataset.choice);
+  });
+});
 
-    if (computerChoice === userChoice) {
-      roundResult = "Tie";
-    } else if (rules[userChoice] === computerChoice) {
-      roundResult = "You won the round";
-      userScore++;
-    } else {
-      roundResult = "Computer won the round";
-      computerScore++;
-    }
+function playRound(userChoice) {
+  let computerChoice = getComputerChoice();
+  let roundResult;
 
-    const roundMessage = `Round ${round}: ${roundResult}. You chose ${userChoice}, computer chose ${computerChoice}.`;
-    console.log(roundMessage);
-    alert(roundMessage);
-  }
-
-  for (let round = 1; round <= roundCount; round++) {
-    const computerChoice = getComputerChoice();
-    const userChoice = getUserChoice();
-    playRound(round, computerChoice, userChoice);
-  }
-
-  let gameResult;
-
-  if (computerScore === userScore) {
-    gameResult = "Tie";
-  } else if (userScore > computerScore) {
-    gameResult = "You won the game";
+  if (computerChoice === userChoice) {
+    roundResult = "Tie";
+  } else if (rules[userChoice] === computerChoice) {
+    roundResult = "You won the round";
   } else {
-    gameResult = "Computer won the game";
+    roundResult = "Computer won the round";
   }
 
-  const gameMessage = `${gameResult}. Score: ${userScore}-${computerScore}.`;
-  console.log(gameMessage);
-  alert(gameMessage);
+  const roundMessage = `Round result: ${roundResult}. You chose ${userChoice}, computer chose ${computerChoice}.`;
+  console.log(roundMessage);
+  alert(roundMessage);
 }
-
-function startGame() {
-  do {
-    playGame();
-  } while (confirm("Do you want to restart the game?"));
-}
-
-startGame();
